585bf2f3df9b9f481ae9d39ac7b72c61607d5ddf
Yen-Jung Luo <loblivious.ee@gmail.com>
2025-10-16 10:00:19 +0800

    implemented endpoint for creating a beer object; added the spring devtool dependency

--- src/main/java/com/loblivious/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/loblivious/spring6restmvc/controller/BeerController.java
@@ -6,8 +6,12 @@ import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +22,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class BeerController {
 
   private final BeerService beerService;
+
+  @PostMapping
+  public ResponseEntity<Beer> createBeer(@RequestBody Beer beer) {
+    log.info("Received Beer post request: {}", beer);
+
+    Beer savedBeer = beerService.saveNewBeer(beer);
+    return new ResponseEntity<>(savedBeer, HttpStatus.CREATED);
+  }
 
   @GetMapping
   public List<Beer> listBeers() {
