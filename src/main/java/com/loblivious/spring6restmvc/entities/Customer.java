b1bf3778dc1f79848a0d4e44683e0f07955f9355
Yen-Jung Luo <loblivious.ee@gmail.com>
2025-11-02 14:33:30 +0800

    tried to fix syntax error that works fine with h2 but not with mysql

--- src/main/java/com/loblivious/spring6restmvc/entities/Customer.java
+++ src/main/java/com/loblivious/spring6restmvc/entities/Customer.java
@@ -26,7 +26,7 @@ public class Customer {
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
-  @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+  @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
   private UUID id;
 
   @NotNull
