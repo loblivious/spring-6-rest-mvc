e8c7100e269862f2589b9cabfc86c4edac6e2253
Yen-Jung Luo <loblivious.ee@gmail.com>
2025-11-03 15:01:58 +0800

    fix tests; import the BeerCscServiceImpl to the test class since test splice doesn't know the context

--- src/test/java/com/loblivious/spring6restmvc/bootstrap/BootstrapDataTest.java
+++ src/test/java/com/loblivious/spring6restmvc/bootstrap/BootstrapDataTest.java
@@ -5,13 +5,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.loblivious.spring6restmvc.repositories.BeerRepository;
 import com.loblivious.spring6restmvc.repositories.CustomerRepository;
 import com.loblivious.spring6restmvc.services.BeerCsvService;
+import com.loblivious.spring6restmvc.services.BeerCsvServiceImpl;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 @DataJpaTest
+@Import(BeerCsvServiceImpl.class)
 class BootstrapDataTest {
 
   @Autowired
@@ -35,7 +38,7 @@ class BootstrapDataTest {
   void testRun() {
     bootstrapData.run();
 
-    assertThat(beerRepository.count()).isEqualTo(3);
+    assertThat(beerRepository.count()).isEqualTo(2413);
     assertThat(customerRepository.count()).isEqualTo(3);
   }
 }
