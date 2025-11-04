7538341742ba6da1056c865f67361a0ab26cb6a9
Yen-Jung Luo <loblivious.ee@gmail.com>
2025-11-04 18:06:42 +0800

    changed page param from 0-based to 1-based

---
+++ src/main/java/com/loblivious/spring6restmvc/configs/WebConfig.java
@@ -0,0 +1,21 @@
+package com.loblivious.spring6restmvc.configs;
+
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+    PageableHandlerMethodArgumentResolver pageableResolver =
+        new PageableHandlerMethodArgumentResolver();
+
+    pageableResolver.setOneIndexedParameters(true);
+
+    resolvers.add(pageableResolver);
+  }
+}
